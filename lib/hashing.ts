3cd9f3cc39bacb2ef77e54ff41dfdbd0daaed6c4
Johanna Herrmann <johanna.herrmann@mailbox.org>
2026-02-11 00:12:59 +0100

    fix: uses node-argon2 for node

--- lib/hashing.ts
+++ lib/hashing.ts
@@ -1,10 +1,37 @@
-import { hash } from 'argon2-browser';
 import { createKey } from '@/createKey';
 import { encode } from '@/base64';
 
 const createNewSalt = async function () {
   const key = await createKey({ sizeInBytes: 16 });
   return encode(key);
+};
+
+const hashWithArgon2Node = async function (password: string, salt: string, sizeInBytes: number): Promise<Uint8Array> {
+  const argon2Node = await import('argon2');
+  const hashEncoded = await argon2Node.hash(password, {
+    salt: Buffer.from(salt, 'utf8'),
+    type: 2,
+    parallelism: 1,
+    hashLength: sizeInBytes,
+    timeCost: 2,
+    memoryCost: 24_576
+  });
+  const hashBase64 = hashEncoded.split('$').at(-1) as string;
+  return new Uint8Array(Buffer.from(hashBase64, 'base64'));
+};
+
+const hashWithArgon2Browser = async function (password: string, salt: string, sizeInBytes: number): Promise<Uint8Array> {
+  const argon2Browser = await import('argon2-browser');
+  const hashHex = await argon2Browser.hash({
+    salt,
+    pass: password,
+    type: 2,
+    parallelism: 1,
+    hashLen: sizeInBytes,
+    time: 2,
+    mem: 24_576
+  });
+  return hashHex.hash;
 };
 
 /**
@@ -24,8 +51,13 @@ const derivePasswordKey = async function ({
   salt?: string;
 }): Promise<[string, Uint8Array]> {
   salt = salt || (await createNewSalt());
-  const result = await hash({ salt, hashLen: sizeInBytes, pass: password, time: 2, parallelism: 1, mem: 24_576 });
-  return [salt, result.hash];
+  let hash: Uint8Array;
+  if (typeof window !== 'undefined') {
+    hash = await hashWithArgon2Browser(password, salt, sizeInBytes);
+  } else {
+    hash = await hashWithArgon2Node(password, salt, sizeInBytes);
+  }
+  return [salt, hash];
 };
 
 /**
--- tests/hashing.spec.ts
+++ tests/hashing.spec.ts
@@ -1,53 +1,5 @@
-import { derivePasswordKey, hashPassword } from '../lib/hashing';
-import { encode } from '../lib/base64';
-
-jest.mock('argon2-browser', () => {
-  return {
-    async hash({
-      salt,
-      hashLen,
-      pass,
-      time,
-      parallelism,
-      mem
-    }: {
-      salt: string;
-      hashLen: number;
-      pass: string;
-      time: number;
-      parallelism: number;
-      mem: number;
-    }) {
-      const key = new Uint8Array([
-        salt.length,
-        hashLen,
-        pass.length,
-        time,
-        parallelism,
-        Math.round(mem / 1024),
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0
-      ]);
-      return { hash: key, hashHex: '', encoded: '' };
-    }
-  };
-});
+import { derivePasswordKey, hashPassword } from '@/hashing';
+import { encode } from '@/base64';
 
 jest.mock('@/createKey', () => {
   return {
@@ -63,7 +15,8 @@ describe('hashing', () => {
 
     expect(salt).toEqual('AAAAAAAAAAAAAAAAAAAAAA==');
     expect(key).toBeInstanceOf(Uint8Array);
-    expect(key).toEqual(new Uint8Array([24, 24, 9, 2, 1, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]));
+    expect(key.length).toBe(24);
+    expect(key).toEqual(new Uint8Array([24, 27, 61, 130, 166, 76, 158, 118, 62, 106, 57, 57, 176, 85, 85, 199, 154, 35, 41, 53, 29, 249, 240, 0]));
   });
 
   it('derivePasswordKey creates key from password, using given salt.', async () => {
@@ -71,20 +24,27 @@ describe('hashing', () => {
 
     expect(salt).toEqual('givenSalt');
     expect(key).toBeInstanceOf(Uint8Array);
-    expect(key).toEqual(new Uint8Array([9, 24, 9, 2, 1, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]));
+    expect(key.length).toBe(24);
+    expect(key).toEqual(
+      new Uint8Array([150, 18, 92, 142, 158, 233, 71, 53, 84, 159, 186, 179, 60, 67, 184, 35, 231, 221, 88, 206, 15, 129, 170, 218])
+    );
   });
 
   it('hashPassword creates hash from password, using new salt.', async () => {
     const [salt, hash] = await hashPassword({ password: 'p8ssw0rd!', sizeInBytes: 24 });
 
     expect(salt).toEqual('AAAAAAAAAAAAAAAAAAAAAA==');
-    expect(hash).toEqual(encode(new Uint8Array([24, 24, 9, 2, 1, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])));
+    expect(hash).toEqual(
+      encode(new Uint8Array([24, 27, 61, 130, 166, 76, 158, 118, 62, 106, 57, 57, 176, 85, 85, 199, 154, 35, 41, 53, 29, 249, 240, 0]))
+    );
   });
 
   it('hashPassword creates hash from password, using given salt.', async () => {
     const [salt, hash] = await hashPassword({ password: 'p8ssw0rd!', sizeInBytes: 24, salt: 'givenSalt' });
 
     expect(salt).toEqual('givenSalt');
-    expect(hash).toEqual(encode(new Uint8Array([9, 24, 9, 2, 1, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])));
+    expect(hash).toEqual(
+      encode(new Uint8Array([150, 18, 92, 142, 158, 233, 71, 53, 84, 159, 186, 179, 60, 67, 184, 35, 231, 221, 88, 206, 15, 129, 170, 218]))
+    );
   });
 });
