781918fd7b727489286f84210a170730a5ef4403
Johanna Herrmann <johanna.herrmann@mailbox.org>
2026-03-01 21:34:41 +0100

    fix: adds wrapping for private encryption key

--- lib/asymmetric/wrapping.ts
+++ lib/asymmetric/wrapping.ts
@@ -1,4 +1,4 @@
-import { arrayBufferToPem, pemToArrayBuffer, validatePrivateSigningKey } from '@/asymmetric/util';
+import { arrayBufferToPem, pemToArrayBuffer, validatePrivateEncryptionKey, validatePrivateSigningKey } from '@/asymmetric/util';
 import { encrypt, decrypt } from '@/symmetric';
 
 /**
@@ -8,7 +8,15 @@ import { encrypt, decrypt } from '@/symmetric';
  * @param encode - boolean, if wrapped key should be base64-encoded
  * @returns Promise with wrappedKey, as base64-encoded string if `encode` is true, as Uint8Array if not
  */
-const wrapPrivateSigningKey = async function ({ privateSigningKey, key, encode }: { privateSigningKey: string; key: Uint8Array, encode?: boolean }): Promise<string> {
+const wrapPrivateSigningKey = async function ({
+  privateSigningKey,
+  key,
+  encode
+}: {
+  privateSigningKey: string;
+  key: Uint8Array;
+  encode?: boolean;
+}): Promise<string> {
   validatePrivateSigningKey(privateSigningKey);
   const dataArrayBuffer = pemToArrayBuffer(privateSigningKey);
   const content = new Uint8Array(dataArrayBuffer);
@@ -18,7 +26,7 @@ const wrapPrivateSigningKey = async function ({ privateSigningKey, key, encode }
 /**
  * unwraps/decrypts private key for sign and verify
  * @param wrappedPrivateSigningKey - key to unwrap/decrypt (Uint8Array or base64-encoded string)
- * @param key - key used to encrypt the keys (length (in bytes) must be 16, 24 or 32 bytes)
+ * @param key - key used to encrypt the key (length (in bytes) must be 16, 24 or 32 bytes)
  * @returns Promise with unwrapped key (Uint8Array)
  */
 const unwrapPrivateSigningKey = async function ({
@@ -33,4 +41,44 @@ const unwrapPrivateSigningKey = async function ({
   return arrayBufferToPem(dataArrayBuffer, 'PRIVATE KEY');
 };
 
-export { wrapPrivateSigningKey, unwrapPrivateSigningKey };
+/**
+ * wraps/encrypts private key for encryption and decryption
+ * @param privateEncryptionKey - private key (PEM, RSA-OAEP)
+ * @param key - key used to encrypt the private key (length (in bytes) must be 16, 24 or 32 bytes)
+ * @param encode - boolean, if wrapped key should be base64-encoded
+ * @returns Promise with wrappedKey, as base64-encoded string if `encode` is true, as Uint8Array if not
+ */
+const wrapPrivateEncryptionKey = async function ({
+  privateEncryptionKey,
+  key,
+  encode
+}: {
+  privateEncryptionKey: string;
+  key: Uint8Array;
+  encode?: boolean;
+}): Promise<string | Uint8Array> {
+  validatePrivateEncryptionKey(privateEncryptionKey);
+  const dataArrayBuffer = pemToArrayBuffer(privateEncryptionKey);
+  const content = new Uint8Array(dataArrayBuffer);
+  return await encrypt({ content, key, encode: encode ?? false, additionalData: ['privateEncryptionKey'] });
+};
+
+/**
+ * unwraps/decrypts private key for encryption and decryption
+ * @param wrappedPrivateEncryptionKey - key to unwrap/decrypt (Uint8Array or base64-encoded string)
+ * @param key - key used to encrypt the key (length (in bytes) must be 16, 24 or 32 bytes)
+ * @returns Promise with unwrapped key (Uint8Array)
+ */
+const unwrapPrivateEncryptionKey = async function ({
+  wrappedPrivateEncryptionKey,
+  key
+}: {
+  wrappedPrivateEncryptionKey: string | Uint8Array;
+  key: Uint8Array;
+}): Promise<string> {
+  const content = (await decrypt({ content: wrappedPrivateEncryptionKey, key, additionalData: ['privateEncryptionKey'] })) as Uint8Array;
+  const dataArrayBuffer = content.buffer;
+  return arrayBufferToPem(dataArrayBuffer, 'PRIVATE KEY');
+};
+
+export { wrapPrivateSigningKey, unwrapPrivateSigningKey, wrapPrivateEncryptionKey, unwrapPrivateEncryptionKey };
